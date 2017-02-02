6895cf99368df07a1d1751f55666480b70344c5d
Robert Ontiu <ontiu.robert@gmail.com>
2017-02-02 14:20:25 +0100

    fix 1.0.3

--- src/crud/src/Contracts/Controller.php
+++ src/crud/src/Contracts/Controller.php
@@ -201,7 +201,7 @@ abstract class Controller extends BaseController
         return [];
     }
 
-    protected function registerIndexFilters(ViewHelper $crud)
+    protected function registerIndexFilters()
     {
         foreach ($this->indexFilters as $filter) {
             $this->store->addFilter(function ($query, Request $request) use ($filter) {
