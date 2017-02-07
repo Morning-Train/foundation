ab221dc78fba7b57d457ac9739fe73a06d259a03
Robert Ontiu <ontiu.robert@gmail.com>
2017-02-07 13:09:52 +0100

    fixed existing fields

--- src/admin/resources/views/crud/fields/input.blade.php
+++ src/admin/resources/views/crud/fields/input.blade.php
@@ -2,6 +2,6 @@
     <label for="{{ $field->id }}">{{ $field->label }}</label>
     {!! call_user_func(
             [ Form::class, $field->options->get('type', 'text') ],
-            $field->name, old($field->name), $field->attributes
+            $field->name, old($field->name, $value), $field->attributes
     ) !!}
 </div>
--- src/admin/resources/views/crud/fields/select.blade.php
+++ src/admin/resources/views/crud/fields/select.blade.php
@@ -1,4 +1,4 @@
 <div class="input-{{ $field->options->get('size', '100') }}">
     <label for="{{ $field->id }}">{{ $field->label }}</label>
-    {!! Form::select($field->name, $options, $value, $field->attributes) !!}
+    {!! Form::select($field->name, $options, old($field->name, $value), $field->attributes) !!}
 </div>
