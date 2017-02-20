d9625ddcee87fc8b638ae9b515e43c0f452b3b5d
Robert Ontiu <ontiu.robert@gmail.com>
2017-02-20 14:28:55 +0100

    enabled files on crud forms

--- src/crud/resources/views/crud/form.blade.php
+++ src/crud/resources/views/crud/form.blade.php
@@ -2,13 +2,14 @@
 
 @section('content-inner')
     <div class="form-container">
-        {!! Form::model($entry, [ 'route' => [ $crud->routeName('store'), $entry->id ] ]) !!}
+        {!! Form::model($entry, [ 'route' => [ $crud->routeName('store'), $entry->id ], 'files' => true ]) !!}
         @if(!$crud->fields->isEmpty())
             @foreach($crud->fields as $field)
                 {!! $field->render($entry, $crud) !!}
             @endforeach
         @endif
-        <input type="submit" value="{{ $entry->isNew() ? $crud->trans('buttons.create') : $crud->trans('buttons.update') }}"/>
+        <input type="submit"
+               value="{{ $entry->isNew() ? $crud->trans('buttons.create') : $crud->trans('buttons.update') }}"/>
         {!! Form::close() !!}
     </div>
 @overwrite
