b736bba7c42b5c0289aa2e94dc28cbc094fa7dc1
Robert Ontiu <ontiu.robert@gmail.com>
2017-03-06 11:02:53 +0100

    fixed reset password notification url

--- src/acl/src/Models/User.php
+++ src/acl/src/Models/User.php
@@ -9,8 +9,20 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use morningtrain\Acl\Notifications\ResetPassword as ResetPasswordNotification;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
     use Authenticatable, Authorizable, CanResetPassword;
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
 }
